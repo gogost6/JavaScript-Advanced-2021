da50c602b32c6eb1e14622c7e989a321b440e8c1
da.sponge <gogost6@gmail.com>
2021-01-25 16:18:10 +0200

    commit

---
+++ 04 DOM INTRODUCTION - EXERCISE/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,23 @@
+function solve() {
+  let input = document.getElementById('text').value.toLowerCase().split(' ');
+  let currentCase = document.getElementById('naming-convention').value;
+  if (currentCase == 'Camel Case') {
+    for (let i = 1; i < input.length; i++) {
+      let element = input[i].split('');
+      let a = element[0].toUpperCase();
+      element[0] = a;
+      input[i] = element.join('');
+    }
+    document.getElementById('result').textContent = input.join('');
+  } else if (currentCase == 'Pascal Case') {
+    for (let i = 0; i < input.length; i++) {
+      let element = input[i].split('');
+      let a = element[0].toUpperCase();
+      element[0] = a;
+      input[i] = element.join('');
+    }
+    document.getElementById('result').innerHTML  = input.join('');
+  } else {
+    document.getElementById('result').innerHTML = 'Error!'
+  }
+}
