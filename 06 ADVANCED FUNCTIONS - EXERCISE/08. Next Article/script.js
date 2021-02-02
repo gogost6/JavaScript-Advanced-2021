02898877172b5023ad05b8ce4bc126d9c9fd88f7
da.sponge <gogost6@gmail.com>
2021-02-02 19:13:00 +0200

    Initial commit!

---
+++ 06 ADVANCED FUNCTIONS - EXERCISE/08. Next Article/script.js
@@ -0,0 +1,10 @@
+function getArticleGenerator(articles) {
+
+    return () => {
+        if (articles.length > 0) {
+            let article = document.createElement("article");
+            article.textContent = articles.shift();
+            document.getElementById("content").appendChild(article);
+        }
+    }
+}
