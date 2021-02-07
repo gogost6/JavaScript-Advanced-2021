85ebbe6b32082b991b9faab9ba3b8d34971bd00e
Georgi Stoilkov <64775995+gogost6@users.noreply.github.com>
2021-02-07 12:50:21 +0200

    Delete Exercise/01_revision directory

--- Exercise/01_revision/01_fruit.js
+++
@@ -1,9 +0,0 @@
-function solve(a,b,c) {
-    console.log(`I need $${((b/1000)*c).toFixed(2)} to buy ${(b/1000).toFixed(2)} kilograms ${a}.`);
-}
-solve('orange', 2500, 1.80);
-<<<<<<< HEAD
-solve('apple', 1563, 2.35)
-=======
-solve('apple', 1563, 2.35)
->>>>>>> 5447f1c2ff3ec88df240dbcc58ffdf349d219dbf
