33193b262628d1ca462a6d69f08df235991ba402
da.sponge <gogost6@gmail.com>
2021-02-09 13:35:46 +0200

    commit

---
+++ Exercise/07 UNIT TESTING AND ERROR HANDLING - EXERCISE/06.Dynamic-Validation/app.js
@@ -0,0 +1,11 @@
+function validate() {
+    const input = document.getElementById('email');
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/g;
+    input.addEventListener('change', () => {
+        if (!pattern.test(input.value)) {
+            input.classList.add('error')
+        } else {
+            input.removeAttribute('class');
+        }
+    })
+}
