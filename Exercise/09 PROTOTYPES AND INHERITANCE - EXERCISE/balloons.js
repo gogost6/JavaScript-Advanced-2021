a80056d47501c863b4e0d1fc985eb9e89361ba38
da.sponge <gogost6@gmail.com>
2021-02-13 19:21:53 +0200

    commit

---
+++ Exercise/09 PROTOTYPES AND INHERITANCE - EXERCISE/balloons.js
@@ -0,0 +1,45 @@
+function createBaloons() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight)
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            }
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}
+
+let classes = createBaloons();
+
+let test = new classes.BirthdayBalloon("Tumno-bqlo", 20.5, "Svetlo-cherno", 10.25, "Happy Birthday!!!");
+let text = test.text;
+console.log(text);
