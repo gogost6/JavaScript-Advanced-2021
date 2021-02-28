e440a8e03164c3e16e6118a58e7c3673c6a1ab9d
da.sponge <gogost6@gmail.com>
2021-02-28 16:14:28 +0200

    Task solved

--- Application/Exercise/Asynchronous-Programming/01.Bus-Stop/app.js
+++ Application/Exercise/Asynchronous-Programming/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const input = document.getElementById('stopId');
+    const id = input.value;
+
+    const url = 'http://localhost:3030/jsonstore/bus/businfo/' + id;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        document.getElementById('stopName').textContent = data.name;
+        const ul = document.getElementById('buses');
+        ul.innerHTML = ''; 
+
+        Object.entries(data.buses).map(([bus, time]) => {
+            const result = document.createElement('li');
+            result.textContent = `Bus ${bus} arrives in ${time} minutes`;
+
+            ul.appendChild(result);
+        })
+
+        input.value = '';
+    } catch (error){
+        const ul = document.getElementById('buses');
+        ul.innerHTML = ''; 
+        document.getElementById('stopName').textContent = 'Error';
+    }
 }
