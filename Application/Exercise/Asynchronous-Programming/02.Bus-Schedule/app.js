cc5f8cc3b564e6a913e79992041d5ed2f1fe5733
da.sponge <gogost6@gmail.com>
2021-03-01 19:07:15 +0200

    Commit

--- Application/Exercise/Asynchronous-Programming/02.Bus-Schedule/app.js
+++ Application/Exercise/Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,26 @@
 function solve() {
-
-    function depart() {
-        console.log('Depart TODO...');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    const info = document.querySelector('#info span');
+    
+    let stopId = {
+        next: 'depot'
+    }
+    
+    async function depart() {
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stopId.next;
+        const response = await fetch(url);
+        const data = await response.json();
+        info.textContent = `Next stop ${data.name}`;
+        stopId = data;
+        departButton.disabled = true;
+        arriveButton.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${stopId.name}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
