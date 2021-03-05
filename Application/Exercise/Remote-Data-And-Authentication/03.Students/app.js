711ec583fe48c54a682505d61e1be90b54918c79
da.sponge <gogost6@gmail.com>
2021-03-05 11:42:12 +0200

    Task completed!

--- Application/Exercise/Remote-Data-And-Authentication/03.Students/app.js
+++ Application/Exercise/Remote-Data-And-Authentication/03.Students/app.js
@@ -1,1 +1,63 @@
-console.log('TODO...');
+const tbody = document.getElementById('tbody');
+async function getStudents() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const responce = await fetch(url);
+    const data = await responce.json();
+    Object.values(data).forEach(x => {
+        dom(x);
+    });
+    document.getElementById('results').appendChild(document.createElement('tfoot'));
+}
+getStudents();
+
+document.getElementById('submit').addEventListener('click', function (e) {
+    e.preventDefault();
+    newStudent();
+})
+
+
+async function newStudent() {
+    let obj = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        facultyNumber: document.getElementById('facultyNumber').value,
+        grade: Number(document.getElementById('grade').value)
+    };
+    if(Object.values(obj).every(x => x != '' || 'null')) {
+        postData(obj);
+        tbody.innerHTML = '';
+        getStudents();
+    } else {
+        return;
+    }
+}
+
+async function postData(data) {
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students', {
+        method: 'post',
+        body: JSON.stringify({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            facultyNumber: data.facultyNumber,
+            grade: data.grade
+        })
+    });
+}
+
+function dom(x) {
+    const tr = document.createElement('tr');
+    const firstName = createEl('th', x.firstName);
+    const lastName = createEl('th', x.lastName);
+    const facultyNumber = createEl('th', x.facultyNumber);
+    const grade = createEl('th', x.grade);
+    tr.append(firstName, lastName, facultyNumber, grade);
+    tbody.appendChild(tr);
+}
+
+function createEl(type, textContent) {
+    let element = document.createElement(type);
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    return element;
+}
