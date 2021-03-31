595df029009cba0881ea639581afafa9f8a84c9c
da.sponge <gogost6@gmail.com>
2021-03-31 15:38:39 +0300

    server update

--- Application/Exercise/Routing/01.Furniture/src/app.js
+++ Application/Exercise/Routing/01.Furniture/src/app.js
@@ -10,7 +10,8 @@ import { myFurniturePage } from './views/myFurniture.js';
 import { registerPage } from './views/register.js';
 
 
-const main = document.querySelector('.container')
+const main = document.querySelector('.container');
+document.querySelector('a#logoutBtn').addEventListener('click', onLogout);
 
 page('/', renderMiddleware, dashboardPage);
 page('/dashboard', renderMiddleware, dashboardPage);
@@ -21,9 +22,28 @@ page('/edit/:id', renderMiddleware, editPage);
 page('/login', renderMiddleware, loginPage);
 page('/register', renderMiddleware, registerPage);
 
+setUserNav();
 page.start();
 
 function renderMiddleware(ctx, next) {
     ctx.render = (context) => render(context, main);
+	ctx.setUserNav = setUserNav;
     next();
+}
+
+function setUserNav() {
+    if (sessionStorage.getItem('userId') != null) {
+        document.querySelector('div.profile span').textContent = `Welcome, ${sessionStorage.getItem('email')}`;
+        document.querySelector('nav div.user').style.display = 'block';
+        document.querySelector('nav div.guest').style.display = 'none';
+    } else {
+        document.querySelector('nav div.user').style.display = 'none';
+        document.querySelector('nav div.guest').style.display = 'block';
+    }
+}
+
+async function onLogout() {
+    await logout();
+    setUserNav();
+    page.redirect('/');
 }
